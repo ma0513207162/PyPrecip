c7a7396088b4dd720844ffad4885cb639c3a3167
pingchagk <2568142759@qq.com>
2024-04-10 19:36:25 +0800

    decorator装饰器的添加

--- pyprecip/__init__.py
+++ pyprecip/__init__.py
@@ -1,13 +1,3 @@
-import os, csv 
 
 
-def read_csv(csv_path):
-    if not os.path.exists(csv_path):  
-        raise FileExistsError
 
-    reader_list: list = []; 
-    with open(csv_path, "r") as csv_file:
-        reader = csv.reader(csv_file)           # reader object 
-        reader_list = list(reader)
-        
-    return reader_list 
--- pyprecip/reading/read_file.py
+++ pyprecip/reading/read_file.py
@@ -1,27 +1,41 @@
 import os, csv
 
-def read_excel(excel_path: str):
-    """
-    workbook = xlrd.open_workbook('test.xlsx')
-    sheet = workbook.sheet_by_name('Sheet1')
-    value = sheet.cell_value(1, 1)
-
-    # 打印值
-    print(value)
-    """
-    pass 
+# decorator
+def file_exists(func):
+    def wrapper(*args, **kwargs):
+        csv_path = args[0]
+        if not os.path.exists(csv_path):
+            raise FileExistsError(f"File '{csv_path}' does not exist")
+        return func(*args, **kwargs)
+    return wrapper
 
 
+
+@file_exists
 def read_csv(csv_path: str) -> list:
-    if not os.path.exists(csv_path):  
-        raise FileExistsError
-
     reader_list: list = []
+
     with open(csv_path, "r") as csv_file:
         reader = csv.reader(csv_file)           # reader object 
         reader_list = list(reader)
         
     return reader_list
+
+
+
+@file_exists
+def read_excel(excel_path: str) -> list:
+    reader_list:list = []
+
+    with open(excel_path, "r") as excel_file:
+        result = excel_file.read()
+        print(result)
+
+
+
+if __name__ == "__main__": 
+    csv_path = "./static/weather_data2.csv"
+    print(read_csv(csv_path))
 
 
 
