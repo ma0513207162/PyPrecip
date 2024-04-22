3eb800643576816ba30a668f77821a4d3e541fdc
pingchagk <2568142759@qq.com>
2024-04-22 21:13:12 +0800

    read excel

--- pyprecip/reading/read_file.py
+++ pyprecip/reading/read_file.py
@@ -1,5 +1,5 @@
 from openpyxl import load_workbook
-import os, csv
+import os, csv, numpy
 
 
 def file_exists(func):
@@ -14,20 +14,33 @@ def file_exists(func):
 
 @file_exists
 def read_excel(excel_path: str, sheet_name: str = "Sheet1") -> list:
+    """
+    How to read an excel file
+    - excel_path (str): specifies the path to the excel file
+    - sheet_name (str): indicates the name of an excel table, The default value is Sheet1
+    list: Returns a two-dimensional list of read completions
+    """
+    
     reader_list:list = []
 
-    workbook = load_workbook(filename = excel_path)
+    workbook = load_workbook(filename = excel_path, data_only=True)
     sheet = workbook[sheet_name]
 
-    for row in sheet.iter_rows(min_row=1, max_row=1, min_col=1, max_col=1):
-        for cell in row:
-            print(cell.value)
+    for row in sheet.iter_rows(values_only=True):
+        reader_list.append(list(row))
 
+    return reader_list; 
 
-
+                
 
 @file_exists
 def read_csv(csv_path: str) -> list:
+    """
+    How to read a csv file
+    -csv_path (str): indicates the path of the csv file
+    list: Returns a two-dimensional list of read completions
+    """
+
     reader_list: list = []
 
     with open(csv_path, "r") as csv_file:
@@ -38,8 +51,11 @@ def read_csv(csv_path: str) -> list:
     
 
 if __name__ == "__main__": 
-    csv_path = "./static/weather_data.csv"
-    reader_list = read_csv(csv_path); 
+    # csv_path = "./static/weather_data.csv"
+    # reader_list = read_csv(csv_path);
+
+    excel_path = "./static/test_data.xlsx"
+    read_excel(excel_path);  
 
 
     
