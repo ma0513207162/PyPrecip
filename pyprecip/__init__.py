2d3c1b66b7d01486539202496a416dddffe0803d
pingchagk <2568142759@qq.com>
2024-04-29 20:09:23 +0800

    更新 read_api

---
+++ pyprecip/__init__.py
@@ -0,0 +1,5 @@
+json_data = ["hello", "world", "world2", "hello"]
+target_ = "hello"
+
+
+print(json_data.index("hello"))
--- pyprecip/reading/read_api.py
+++ pyprecip/reading/read_api.py
@@ -1,6 +1,11 @@
 import requests, json 
 from requests.exceptions import Timeout, RequestException 
 
+def raise_exception(message = "error", exception_type = Exception):
+    
+    raise exception_type(message) 
+
+
 def send_request(URL: str, PARAMS: dict):
     MAX_RETRIES = 3     # 最大重试次数 
     retry_count = 0     
@@ -13,16 +18,18 @@ def send_request(URL: str, PARAMS: dict):
             retry_count += 1 
             print(f"Request timed out, retrying... (Attempt {retry_count}/{MAX_RETRIES})")
         except RequestException as e:
-            raise RequestException(f"An error occurred: {e}")
+            raise_exception(f"An error occurred: {e}", RequestException)
     else:
-        raise RequestException("Maximum number of retries reached, giving up.")
+        raise_exception("Maximum number of retries reached, giving up.", RequestException)
 
 
-def get_weather_data(area_code: int = -1, forecasts: bool = False) -> dict:
-    if type(area_code) != int:
-        raise TypeError("The area_code parameter type must be int.") 
-    if type(forecasts) != bool:
-        raise TypeError("The type of forecasts parameter must be bool.") 
+def get_weather_data(area_code: int = -1, area_name: str = "", forecasts: bool = False) -> dict:
+    if not isinstance(area_code, int):
+        raise_exception("area_code parameter must be of type int.", TypeError)
+    if not isinstance(area_name, str):
+        raise_exception("area_name parameter must be of type str.", TypeError)
+    if not isinstance(forecasts, bool): 
+        raise_exception("forecasts parameter must be of type bool.", TypeError)
             
     request_result: dict = {}
     with open("./static/constant.json", "r") as file:
@@ -40,7 +47,7 @@ def get_weather_data(area_code: int = -1, forecasts: bool = False) -> dict:
             area_code = location_data["adcode"]
             request_result["area_code"] = location_data["adcode"]
         else:
-            raise RequestException("An unknown error occurred with the ip location request. Please try again later")
+            raise_exception("An unknown error occurred with the ip location request. Please try again later", RequestException)
     else:
         request_result["area_code"] = area_code
 
@@ -71,13 +78,35 @@ def get_weather_data(area_code: int = -1, forecasts: bool = False) -> dict:
         
         return request_result                    
     else:
-        raise RequestException("An unknown error occurred in the climate data request. Please try again later")
+        raise_exception("An unknown error occurred in the climate data request. Please try again later", RequestException)
     
 
+def get_area_code(area_name: str):
+    with open("./static/constant.json", "r", encoding="utf-8") as file:
+        READ_API_DATA: dict = json.load(file)["READ_API"]    
+    DUP_PLACE_NAMES = READ_API_DATA["DUP_PLACE_NAMES"]
+
+    if area_name != "":
+        with open("./static/city_code.json", "r", encoding="utf-8") as file:
+            city_code: dict = json.load(file)
+        city_code_keys = list(city_code.keys()) 
+        city_code_values = list(city_code.values())
+        
+        if len(city_code_keys) == len(city_code_values): 
+            if area_name in DUP_PLACE_NAMES:
+                # 地名不是唯一的 
+                pass 
+            else:
+                # 地名是唯一的 
+                index_ = city_code_values.index(area_name)
+                return city_code_keys[index_]
+        else:
+            raise_exception("Unknown error: len(city_code_keys) != len(city_code_values)");
+    else:
+        raise_exception("area_name parameter cannot be null or invalid.", ValueError)
 
 
 if __name__ == "__main__":
-    result = get_weather_data(forecasts= True)
-    print(result)
+    result = get_area_code("汕头市")
 
 
